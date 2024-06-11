4187024b04c260cd24e768d161269422f536f237
MR <57211612+mryas1105@users.noreply.github.com>
2024-06-12 00:20:56 +0700

    views

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="form-outline mb-4">
+                <label class="form-label" for="form3Example1cg">Your Name</label>
+                  <input type="text" id="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                  @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                  @enderror
+
+                </div>
+
+                <div class="form-outline mb-4">
+                <label class="form-label" for="form3Example3cg">Your Email</label>
+                  <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                  @enderror
+
+                </div>
+
+                <div class="form-outline mb-4">
+                            <label for="password" class="form-label">Password</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <label for="password-confirm" class="form-label">Repeat Password</label>
+
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+
+                <div class="d-flex justify-content-center">
+                  <input type="submit" class="btn btn-success btn-block btn-lg" value="Register">
+                </div>
+                </form>
+                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="{{ route('login') }}" class="fw-bold text-body"><u>Login here</u></a></p>
+
+
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+@endsection
