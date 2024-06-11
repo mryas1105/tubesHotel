d9647ad77c9ecf570e80e5cd223482b4b8984e85
dimasnabil <nabildimas410@gmail.com>
2024-06-11 15:10:18 +0700

    update

--- app/Models/FasilitasKamar.php
+++ app/Models/FasilitasKamar.php
@@ -17,14 +17,4 @@ class FasilitasKamar extends Model
     }
 }
 
-class FasilitasKamar extends Model
-{
-    use HasFactory;
-    protected $table = 'fasilitas_kamars';
-    protected $fillable = ['kamar_id' , 'nama_fasilitas'];
 
-    public function Kamar()
-    {
-        return $this->belongsTo(Kamar::class , 'kamar_id');
-    }
-}
