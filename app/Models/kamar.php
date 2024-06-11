55e800819227d0651124d33575eacfdfdf4fa549
andikarizky05 <andikarizky05@users.noreply.github.com>
2024-06-11 15:16:56 +0700

    Update

--- app/Models/kamar.php
+++ app/Models/kamar.php
@@ -6,3 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
+class kamar extends Model
+{
+    use HasFactory;
+
+    public function harga_format()
+    {
+        return "Rp." . number_format($this->harga_kamar, 0, ",", ".");
+    }
+    public function FasilitasKamar()
+    {
+        return $this->hasMany(FasilitasKamar::class);
+    }
+}
