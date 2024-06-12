cdd21662fcc15bae271893c8752218c6f00f6b36
dimasnabil <nabildimas410@gmail.com>
2024-06-12 10:45:39 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,4 +8,43 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
 
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo(){
+        if(Auth::user()->roles->get(0)->name == 'admin'){
+            return RouteServiceProvider::ADMIN;
+        } else if(Auth::user()->roles->get(0)->name == 'resepsionis') {
+            return RouteServiceProvider::RESEPSIONIS;
+        }
+        return RouteServiceProvider::HOME;
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
