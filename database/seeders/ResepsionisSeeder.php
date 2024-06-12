ab270a5653d0d8f7e133a74c1f900c554bbe9e3c
dimasnabil <nabildimas410@gmail.com>
2024-06-12 10:52:09 +0700

    update

--- database/seeders/ResepsionisSeeder.php
+++ database/seeders/ResepsionisSeeder.php
@@ -5,4 +5,23 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
+class ResepsionisSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user =User::create([
+            'name' => 'resepsionis',
+            'email' => 'resepsionishotel@gmail.com',
+            'password' => bcrypt('12345678'),
+            'email_verified_at' => now(),
+        ]);
+
+        $user->assignRole('resepsionis');
+    }
+}
 
