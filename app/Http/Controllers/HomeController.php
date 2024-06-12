0f3a8d9ab9d2dc057701d9a10064ccccf89d8b10
andikarizky05 <andikarizky05@users.noreply.github.com>
2024-06-12 10:56:47 +0700

    Update Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,3 +6,26 @@ use Illuminate\Http\Request;
 use App\Models\kamar;
 
 
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $kamars = kamar::all();
+        return view('home',compact('kamars'));
+    }
+}
